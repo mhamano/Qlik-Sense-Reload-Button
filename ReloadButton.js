f3be752392c61d926506960f7129e4b0a485346d
Masaki Hamano <masaki.hamano@gmail.com>
2015-10-01 12:24:29 +0900

    Added "Partial Reload" function and renewed UI
    - Added "Partial Reload" function
    - UI was renewed

--- ReloadButton.js
+++ ReloadButton.js
@@ -1,88 +1,97 @@
-define(["jquery", "qlik", "text!./lib/css/reload_btn.css", "text!./lib/css/jquery-ui.min.css", "./lib/js/jquery-ui.min"], function($, qlik, cssContent, jqueryUICSS, jqueryUI) {
+define(["jquery", "qlik", "text!./lib/css/reload_btn.css"], function($, qlik, cssContent) {
   
     $("<style>").html(cssContent).appendTo("head");
-	$("<style>").html(jqueryUICSS).appendTo("head");
- 
+
 	return {
 		paint: function ($element, layout) {
-                    
-                      
-            var app = qlik.currApp(this);  
-          
-            var html = html = '<a href="#" class="reload_btn">Reload</a><div class="myCustomScope" ></div><div id="show_dialog"></div>'; 
-            $element.html( html );
-			          
-		                       
-            $(".reload_btn").click(function(event){             
-              	event.preventDefault();                          
-				ShowJQueryConfirmDialog();              
-            });
+		                                           
+            var app = qlik.currApp(this);  	
+		  
+		    //Check if Qlik Sense Desktop or Server
+		  	var isPersonalMode = true;
+		  
+			var global = qlik.getGlobal();
+			global.isPersonalMode( function ( reply ) {
+				isPersonalMode = reply.qReturn;
+			});
 			
-            // Display complete/cancellation message
-			function ShowJQueryAlertDialog( strTitle, strComment ) {	
-				$( "#show_dialog" ).html( strComment );
+		    // Display Extension Visualization
+            var html = html = '<a href="#" id="modal-open" class="btn btn-primary">Reload</a>'; 
+            $element.html( html );
+		
+			// Open modal 
+			$("#modal-open").click(
+				function(event){
+					event.preventDefault(); 
+					
+					$(this).blur() ;	
+					
+					// Check if modal is displayed 
+					if($("#modal-overlay")[0]) return false ;
+					
+					// Add modal overlay
+					$(".qv-panel-sheet").append('<div id="modal-overlay"></div>');
+					$("#modal-overlay").fadeIn("slow");
 
-				$( "#show_dialog" ).dialog({
-					modal: true,
-					title: strTitle,
-					buttons: {
-						"OK": function() {
-							$( this ).dialog( "close" );
+					// Add modal panel
+					$(".qv-panel-sheet").append('<div id="modal-content" style="display:none"><div id="modal-message"><h2>Are you sure to execute reload?</h2></div><div id="modal-checkbox"><input type="checkbox" id="partial" name="partial" value=""><label for="partial">Partial reload</label></div><div id="modal-botton"><a href="#" id="execute-reload" class="btn btn-primary"> OK </a><a href="#" id="modal-close" class="btn btn-default">Cancel</a></div></div>');					
+					$("#modal-content").fadeIn("slow");
+					
+					// Close modal 
+					$("#modal-overlay, #modal-close").unbind().click(
+						function(event){
+							event.preventDefault(); 
+					
+							$("#modal-content,#modal-overlay").fadeOut("slow",function(){
+								$("#modal-content").remove();
+								$("#modal-overlay").remove();
+							});
 						}
-					}
-				});
-			}
-          
-			 // Display confirmation dialogue
-			function ShowJQueryConfirmDialog() {
-				var strTitle = "Confirmation";
-				var strComment = "Are you sure to execute reload?";
-				
-				$( "#show_dialog" ).html( strComment );
-				
-				var d =  $( "#show_dialog" ).dialog({
-                  	//autoOpen: false,
-					modal: true,
-					title: strTitle,
-					buttons: {
-						"OK": function() {
-							$( this ).dialog( "close" );
+					);
+
+					// Execute reload								
+					$("#execute-reload").click(	
+						function(event){
+							event.preventDefault();
 							
-							dispLoading("Reloading...");
-                          
+							// Check if reload is partial
+							var isPartial = false;
+							if($("#partial").prop('checked')) {
+								isPartial = true;
+							}
+													
+							// Remove modal
+							$("#modal-content").remove();
+							
+							// Open loader circle
+							$("#modal-overlay").append('<div class="loader">Loading...</div>');
+							
+							//console.log("isPersonalMode: " + isPersonalMode);
+							//console.log("isPartial: " + isPartial);
+							
 							//Execute reload
-							app.doReload().then(function(){ 
-								app.doSave();
-								removeLoading();
-								ShowJQueryAlertDialog( "Completed", "Reload execution completed" );
-							});							
-						},
-						"Cancel": function() {
-							$( this ).dialog( "close" );
-							ShowJQueryAlertDialog( "Cancelled", "Reload was cancelled." );
-						}
-					}
-				}).parent(".ui-dialog").wrap("<div class='myCustomScope'></div>");
-			}
-	
-            // Start to display loading circle
-			function dispLoading(msg){
-				var dispMsg = "";
- 
-				if( msg != "" ){
-					dispMsg = "<div class='loadingMsg'>" + msg + "</div>";
+							if ( isPersonalMode ) {
+								app.doReload( 0, isPartial, false ).then( function () {
+									app.doSave();
+									$("#modal-overlay").remove();
+								} );
+							} else {
+								// qlik.callRepository( '/qrs/app/' + appid + '/reload', 'POST' ).success( function ( reply ) {
+								//	$("#modal-overlay").remove()
+								// } );
+								app.doReload( 0, isPartial, false ).then( function () {
+									app.doSave();
+									$("#modal-overlay").remove();
+								} );
+							}
+						}										
+					);					
+
+						
 				}
-				
-				if($("#loading").size() == 0){
-					$("body").append("<div id='loading'>" + dispMsg + "</div>");
-				}	
-			}
- 
-            // Stop to display loading circle
-			function removeLoading(){
-				$("#loading").remove();
-			}
+			);
 		}
 	};
 } );
+
 
