7f8e668864401a984a368064b8446ec8a01ab4b8
Masaki Hamano <AMO@qlik.com>
2017-05-11 10:46:54 +0900

    Fixed to be able to execute reloads on QS3.2 SR3

--- ReloadButton.js
+++ ReloadButton.js
@@ -1,47 +1,47 @@
 define(["jquery", "qlik", "text!./lib/css/reload_btn.css"], function($, qlik, cssContent) {
-  
+
     $("<style>").html(cssContent).appendTo("head");
 
 	return {
 		paint: function ($element, layout) {
-		                                           
-            var app = qlik.currApp(this);  	
-		  
+
+            var app = qlik.currApp(this);
+
 		    //Check if Qlik Sense Desktop or Server
 		  	var isPersonalMode = true;
-		  
+
 			var global = qlik.getGlobal();
 			global.isPersonalMode( function ( reply ) {
 				isPersonalMode = reply.qReturn;
 			});
-			
+
 		    // Display Extension Visualization
-            var html = html = '<a href="#" id="modal-open" class="btn btn-primary">Reload</a>'; 
+            var html = html = '<a href="#" id="modal-open" class="btn btn-primary">Reload</a>';
             $element.html( html );
-		
-			// Open modal 
+
+			// Open modal
 			$("#modal-open").click(
 				function(event){
-					event.preventDefault(); 
-					
-					$(this).blur() ;	
-					
-					// Check if modal is displayed 
+					event.preventDefault();
+
+					$(this).blur() ;
+
+					// Check if modal is displayed
 					if($("#modal-overlay")[0]) return false ;
-					
+
 					// Add modal overlay
 					$(".qv-panel-sheet").append('<div id="modal-overlay"></div>');
 					$("#modal-overlay").fadeIn("slow");
 
 					// Add modal panel
-					$(".qv-panel-sheet").append('<div id="modal-content" style="display:none"><div id="modal-message"><h2>Are you sure to execute reload?</h2></div><div id="modal-checkbox"><input type="checkbox" id="partial" name="partial" value=""><label for="partial">Partial reload</label></div><div id="modal-botton"><a href="#" id="execute-reload" class="btn btn-primary"> OK </a><a href="#" id="modal-close" class="btn btn-danger">Cancel</a></div></div>');					
+					$(".qv-panel-sheet").append('<div id="modal-content" style="display:none"><div id="modal-message"><h2>Are you sure to execute reload?</h2></div><div id="modal-checkbox"><input type="checkbox" id="partial" name="partial" value=""><label for="partial">Partial reload</label></div><div id="modal-botton"><a href="#" id="execute-reload" class="btn btn-primary"> OK </a><a href="#" id="modal-close" class="btn btn-danger">Cancel</a></div></div>');
 					$("#modal-content").fadeIn("slow");
-					
-					// Close modal 
+
+					// Close modal
 					$("#modal-overlay, #modal-close").unbind().click(
 						function(event){
-							event.preventDefault(); 
-					
+							event.preventDefault();
+
 							$("#modal-content,#modal-overlay").fadeOut("slow",function(){
 								$("#modal-content").remove();
 								$("#modal-overlay").remove();
@@ -49,36 +49,36 @@ define(["jquery", "qlik", "text!./lib/css/reload_btn.css"], function($, qlik, cs
 						}
 					);
 
-					// Execute reload								
-					$("#execute-reload").click(	
+					// Execute reload
+					$("#execute-reload").click(
 						function(event){
 							event.preventDefault();
-							
+
 							// Check if reload is partial
 							var isPartial = false;
 							if($("#partial").prop('checked')) {
 								isPartial = true;
 							}
-													
+
 							// Remove modal
 							$("#modal-content").remove();
-							
+
 							// Open loader circle
 							$("#modal-overlay").append('<div id="loader" class="loader">Loading...</div>');
-							
+
 							//console.log("isPersonalMode: " + isPersonalMode);
 							//console.log("isPartial: " + isPartial);
-							
+
 							//Execute reload
 							if ( isPersonalMode ) {
 								app.doReload( 0, isPartial, false ).then( function (e) {
 									$("#loader").remove();
-									if(e.qReturn) {
+									if(e) {
 										app.doSave();
 										$("#modal-overlay").append('<div id="modal-content" style="display:none"><div id="modal-message"><h2>Reload succeeded!</h2></div><br><div id="modal-checkbox"><a href="#" id="modal-close" class="btn btn-success">Close</a></div></div>');
 									} else {
-										$("#modal-overlay").append('<div id="modal-content" style="display:none"><div id="modal-message"><h2>Reload failed!</h2></div><br><div id="modal-checkbox"><a href="#" id="modal-close" class="btn btn-danger">Close</a></div></div>');										
-									}																		
+										$("#modal-overlay").append('<div id="modal-content" style="display:none"><div id="modal-message"><h2>Reload failed!</h2></div><br><div id="modal-checkbox"><a href="#" id="modal-close" class="btn btn-danger">Close</a></div></div>');
+									}
 									$("#modal-content").fadeIn("slow");
 								} );
 							} else {
@@ -87,23 +87,21 @@ define(["jquery", "qlik", "text!./lib/css/reload_btn.css"], function($, qlik, cs
 								// } );
 								app.doReload( 0, isPartial, false ).then( function (e) {
 									$("#loader").remove();
-									if(e.qReturn) {
+									if(e) {
 										app.doSave();
 										$("#modal-overlay").append('<div id="modal-content" style="display:none"><div id="modal-message"><h2>Reload succeeded!</h2></div><br><div id="modal-checkbox"><a href="#" id="modal-close" class="btn btn-success">Close</a></div></div>');
 									} else {
-										$("#modal-overlay").append('<div id="modal-content" style="display:none"><div id="modal-message"><h2>Reload failed!</h2></div><br><div id="modal-checkbox"><a href="#" id="modal-close" class="btn btn-danger">Close</a></div></div>');										
-									}																		
+										$("#modal-overlay").append('<div id="modal-content" style="display:none"><div id="modal-message"><h2>Reload failed!</h2></div><br><div id="modal-checkbox"><a href="#" id="modal-close" class="btn btn-danger">Close</a></div></div>');
+									}
 									$("#modal-content").fadeIn("slow");
 								} );
 							}
-						}										
-					);					
+						}
+					);
 
-						
+
 				}
 			);
 		}
 	};
 } );
-
-
